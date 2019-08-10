471f39a7ca89de32f3ef9cec988e67f332e3a075
Patrick Kranzpiller <pkranzpiller@gmail.com>
2019-08-09 20:27:16 -0500

    created basic api that returns usernames

---
+++ initializedb.sql
@@ -0,0 +1,14 @@
+create table users(id serial primary key, username text, password text, usertype text);
+create table reviews(id serial primary key, rating float, description text, username text);
+
+insert into users(username, password, usertype)  values('username', 'password', 'user');
+insert into users(username, password, usertype) values('Knuckles', 'doyouknowtheway', 'user');
+insert into users(username, password, usertype) values('Jim', 'password', 'user');
+insert into users(username, password, usertype) values('Overlord','callmesenpai', 'admin');
+
+select * from users;
+select * from reviews;
+
+
+drop table users;
+drop table reviews;
